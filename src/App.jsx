a86331b088d87967e980c205e48cd3785c8ec0b7
Alcides Thiago <alcides_thiago@hotmail.com>
2024-07-26 14:12:49 +0000

    Include new card

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ function App() {
         <h1>Java</h1>
         <img src="https://www.salvatore.academy/devmon/1_java.png" width="200" />
       </div>
+      <div className="card">
+        <h1>Kotlin</h1>
+        <img src="https://www.salvatore.academy/devmon/2_kotlin.png" width="200" />
+      </div>
     </>
   )
 }
